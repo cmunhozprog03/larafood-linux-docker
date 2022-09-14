fbbcbd281cdc629fd2d5e117f30e24a42f041f2e
cmunhozprog03 <cmunhozprog03@hotmail.com>
2022-09-14 17:37:04 -0300

    category and subcategory

--- database/migrations/2022_09_14_202656_create_subcategories_table.php
+++ database/migrations/2022_09_14_202656_create_subcategories_table.php
@@ -13,16 +13,16 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('categories', function (Blueprint $table) {
+        Schema::create('subcategories', function (Blueprint $table) {
             $table->id();
-
+            $table->foreignId('category_id')->constrained()->cascadeOnDelete();
+            
             $table->string('name');
             $table->string('slug');
             $table->string('icon')->nullable();
             $table->string('image')->nullable();
             $table->string('thumb')->nullable();
             $table->enum('active', [1 ,2])->default(1);
-
 
             $table->timestamps();
         });
@@ -35,6 +35,6 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('categories');
+        Schema::dropIfExists('subcategories');
     }
 };
